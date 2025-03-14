5e11a65fc55916e7571350b95c2d7196bb2af2f0
nik-din <nik.dindo@gmail.com>
2025-03-15 00:37:55 +0100

    troppa_skill_ma_devo_mangiare_la_torta

--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@ using namespace std;
 
 struct card{
     int color, shape, filling, number;
-    string out(){
+    /* string out(){
         //todo
-    }
+    } */
 };
 
 int main(){
@@ -34,14 +34,73 @@ int main(){
         return false;
     };
 
+    string none = "\033[0m";
+    vector<string> color = {"\033[0;31m", "\033[0;32m", "\033[0;34m"};
+
+    vector<vector<string>> figure= {{
+        "       |",
+        "   *   |",
+        "  ***  |",
+        " ***** |",
+        "  ***  |",
+        "   *   |",
+        "_______|",},
+        {
+       "       |",
+       "  ***  |",
+       " ***** |",
+       " ***** |",
+       " ***** |",
+       "  ***  |",
+       "_______|",
+        },
+        {
+       "       |",
+       "  ***  |",
+       " ***   |",
+       "  ***  |",
+       "   *** |",
+       "  ***  |",
+       "_______|",
+    },
+    };
+    vector<char> fill = {'*', '|', '-'};
+    vector<char> nm = {'1', '2', '3'};
+
     auto output = [&](){
         //todo
+        
+        vector<string> mat(7*3+1, string(8*4+1, ' '));
+        for(int i = 0; i<33; i++){
+            mat[0][i] = '_';
+        }
+        for(int i = 1; i<22; i++){
+            mat[i][0] = '|';
+        }
+        for(int i = 0; i<3; i++){
+            for(int j = 0; j<4; j++){
+                char fl = fill[table[i*4+j].filling];
+                for(int k = 0; k<7; k++){
+                    for(int l = 0; l<8; l++){
+                        mat[i*7+k+1][j*8+l+1] = figure[table[i*4+j].shape][k][l];
+                        if(mat[i*7+k+1][j*8+l+1] == '*') mat[i*7+k+1][j*8+l+1] = fl;
+                    }
+                }
+                mat[i*7+3+1][j*8+3+1] = nm[table[i*4+j].number];
+                //for(int i = 1; i<6; i++) 
+            }
+        }
+        for(int i = 0; i<22; i++){
+            cout << mat[i] << '\n';
+        }
     };
 
     copy(deck.begin(), deck.begin()+12, table.begin());
-    output();
+    
     int idx = 12;
     while(idx < 81){
+        cout.clear();
+        output();
         vector<array<int, 2>> cor(3);
         for(int i = 0; i<3; i++){
             cin >> cor[i][0] >> cor[i][1];
@@ -55,4 +114,7 @@ int main(){
             cout << "non è un set\n";
         }
     }
+    cout.clear();
+    output();
+    cout << "la partita è (circa) finita\n";
 }
